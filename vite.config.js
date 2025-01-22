0c9691eda33d3a36a634ded5b2d7ab053dbe7739
waseta <mayinboo_95@hotmail.com>
2025-01-22 14:44:00 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 export default defineConfig({
   plugins: [svelte()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src'), 
+    },
   },
+
 });
